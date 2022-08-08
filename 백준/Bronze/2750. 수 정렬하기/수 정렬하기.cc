5f2612a4c6b8b33b805c8e553495afccdfc15888
윤혜미 <78525791+YunHye-Mi@users.noreply.github.com>
2022-08-08 14:30:33 +0900

    [Bronze II] Title: 수 정렬하기, Time: 44 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+
+bool compare(const void* a, const void* b) {
+	return *(int*)a - *(int*)b;
+}
+
+int main() {
+	int n, a;
+	vector<int>s;
+
+	vector<int>::iterator it;
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> a;
+		s.push_back(a);
+	}
+
+	sort(s.begin(), s.end());
+
+	s.erase(unique(s.begin(), s.end()), s.end());
+
+	for (it = s.begin(); it != s.end(); ++it)
+		cout << *it << endl;
+	
+	return 0;
+}
