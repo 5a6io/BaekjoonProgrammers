7acacd7f04a092dac327ad6da92c61ef69b4df44
윤혜미 <78525791+YunHye-Mi@users.noreply.github.com>
2023-01-16 13:45:35 +0900

    [level 2] Title: 구명보트, Time: 1.25 ms, Memory: 5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int solution(vector<int> people, int limit) {
+    int answer = 0;
+    vector<int> boat;
+
+    sort(people.begin(), people.end());
+    auto p1 = people.begin();
+    auto p2 = people.end() - 1;
+    
+    while(p1 <= p2){
+        if(*p1 + *p2 > limit){
+            boat.push_back(*p2);
+            p2--;
+        }
+        else if(*p1 + *p2 <= limit){
+            boat.push_back(*p1 + *p2);
+            p1++;
+            p2--;
+        }
+        else if(p1 == p2)
+            boat.push_back(*p1);
+    }
+    return answer = boat.size();
+}
