52bd782f76eabb3ee29c3abed0d23ff1ce17471e
윤혜미 <78525791+YunHye-Mi@users.noreply.github.com>
2023-01-24 15:55:16 +0900

    [level 3] Title: 정수 삼각형, Time: 0.86 ms, Memory: 8.82 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43105. 정수 삼각형/정수 삼각형.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int solution(vector<vector<int>> triangle) {
+    int answer = 0;
+    vector<vector<int>> sum(triangle.size(), vector<int>(triangle.size(), 0));
+    sum[0][0] = triangle[0][0];
+    
+    for(int i = 0; i < triangle.size() - 1; i++)
+        for(int j = 0; j < triangle[i].size(); j++){
+            sum[i+1][j] = max(sum[i+1][j], sum[i][j] + triangle[i+1][j]);
+            sum[i+1][j+1] = max(sum[i+1][j+1], sum[i][j] + triangle[i+1][j+1]);
+        }
+    
+    return answer = *max_element(sum[sum.size()-1].begin(), sum[sum.size()-1].end());
+}
