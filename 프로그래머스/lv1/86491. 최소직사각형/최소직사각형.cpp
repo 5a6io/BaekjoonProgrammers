02bc5d3439a47ab110e1273e21cb319be350f1d6
윤혜미 <78525791+YunHye-Mi@users.noreply.github.com>
2023-01-28 21:43:15 +0900

    [level 1] Title: 최소직사각형, Time: 0.74 ms, Memory: 5.02 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.cpp
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.cpp
@@ -6,19 +6,10 @@ using namespace std;
 
 int solution(vector<vector<int>> sizes) { // sizes 배열을 모두 탐색하여 각 명함의 긴 길이를 앞에 짧은 길이는 뒤에
     int answer = 0;
-    
-    for(int i = 0; i < sizes.size(); i++){
-        if(sizes[i][0] < sizes[i][1]){
-            int temp = sizes[i][0];
-            sizes[i][0] = sizes[i][1];
-            sizes[i][1] = temp;
-        }
-    }
-    
-    int w = sizes[0][0], h = sizes[0][1];
+    int w = 0, h = 0;
     for(int i = 0; i < sizes.size(); i++) {
-        w = max(w, sizes[i][0]);
-        h = max(h, sizes[i][1]);
+        w = max(w, min(sizes[i][0], sizes[i][1]));
+        h = max(h, max(sizes[i][0], sizes[i][1]));
     }
     
     return answer = w * h;
