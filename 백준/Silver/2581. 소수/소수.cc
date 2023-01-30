ac211d3bbc8a93239fae9f9c3af1544a71166218
윤혜미 <78525791+YunHye-Mi@users.noreply.github.com>
2023-01-30 12:48:40 +0900

    [Silver V] Title: 소수, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/2581. 소수/소수.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cstdlib>
+using namespace std;
+
+bool isPrime(int n){
+    if(n == 1)
+        return false;
+    else {
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int m, n, sum = 0;
+    vector<int> prime;
+    cin >> m >> n;
+
+    for (int i = m; i <= n; i++)
+        if(isPrime(i)) {
+            prime.push_back(i);
+            sum += i;
+        }
+
+    if(prime.empty())
+        cout << -1 << endl;
+    else{
+        cout << sum << endl;
+        cout << prime[0] << endl;
+    }
+}
