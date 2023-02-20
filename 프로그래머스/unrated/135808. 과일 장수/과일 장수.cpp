1c0c8e4091c5016abf35f19aaef971526468bafd
윤혜미 <78525791+YunHye-Mi@users.noreply.github.com>
2023-02-20 12:40:23 +0900

    [unrated] Title: 과일 장수, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int k, int m, vector<int> score) {
+    int answer = 0;
+    int i = m - 1;
+    
+    sort(score.begin(), score.end(), greater<int>());
+    
+    while(i < score.size()){
+        answer += score[i] * m;
+        i += m;
+    }
+    
+    return answer;
+}
