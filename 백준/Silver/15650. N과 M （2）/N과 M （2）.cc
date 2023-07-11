3616236a2bc59abbae3d65e4841bc33fd9006b12
윤혜미 <78525791+YunHye-Mi@users.noreply.github.com>
2023-07-11 22:54:47 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n, m; // 1 <= m <= n <= 8
+vector<int> result;
+
+void backtracking(int a) {
+    if (result.size() == m) {
+        for (int i = 0; i < m; i++)
+            cout << result[i] << " ";
+        cout << "\n";
+        return;
+    }
+
+    vector<bool> visit(n+1, false);
+    for (int i = a; i <= n; i++) {
+        if (!visit[i] && find(result.begin(), result.end(), i) == result.end()) {
+            visit[i] = true;
+            result.push_back(i);
+            backtracking(i);
+            result.pop_back();
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n >> m;
+
+    backtracking(1);
+}
