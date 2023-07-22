49c2e2695faca45020546bda46ef03e1f5ced1e2
윤혜미 <78525791+YunHye-Mi@users.noreply.github.com>
2023-07-22 18:39:31 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    priority_queue<int, vector<int>, greater<int>> q;
+    for (int i = 0; i < n; ++i) {
+        int temp;
+        cin >> temp;
+        if (temp == 0) {
+            if (q.empty()) {
+                cout << "0\n";
+            } else {
+                cout << q.top() << "\n";
+                q.pop();
+            }
+        } else {
+            q.push(temp);
+        }
+    }
+}
