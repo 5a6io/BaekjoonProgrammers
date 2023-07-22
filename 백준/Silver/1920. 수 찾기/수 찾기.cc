abb8c010603fc9e736bb2ef5a3efdf0141996aff
윤혜미 <78525791+YunHye-Mi@users.noreply.github.com>
2023-07-22 17:57:20 +0900

    [Silver IV] Title: 수 찾기, Time: 64 ms, Memory: 8132 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -4,23 +4,6 @@
 
 using namespace std;
 
-//int a[100001];
-
-//int binarySearch(int num, int start, int end) {
-//    int mid = (start + end) / 2;
-//
-//    if (start <= end) {
-//        if (num == a[mid]) {
-//            return 1;
-//        } else if (num > a[mid]) {
-//            return binarySearch(num, mid + 1, end);
-//        } else if (num < a[mid]) {
-//            return binarySearch(num, start, mid - 1);
-//        }
-//    }
-//    return 0;
-//}
-
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
