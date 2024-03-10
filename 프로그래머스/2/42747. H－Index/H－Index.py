5b6701999c15fb99ac5d4c1e33c6e3c135a44cb1
윤혜미 <78525791+YunHye-Mi@users.noreply.github.com>
2024-03-10 14:01:14 +0900

    [level 2] Title: H-Index, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -0,0 +1,15 @@
+def solution(citations):
+    answer = 0
+    n = len(citations)
+    citations.sort(reverse=True)
+
+    if citations[0] == 0:
+        return answer
+
+    for h in range(len(citations)):
+        if citations[h] > h:
+            answer += 1
+        else:
+            continue
+
+    return answer
