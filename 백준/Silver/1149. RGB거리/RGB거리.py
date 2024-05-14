849d027c88fdc861d7e89fd51959dd9556947d93
윤혜미 <78525791+YunHye-Mi@users.noreply.github.com>
2024-05-14 19:36:51 +0900

    Update RGB거리.py

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -5,7 +5,7 @@ house[0] = list(map(int, input().split()))
 
 for i in range(1, N):
     a, b, c = map(int, input().split())
-    house[i][0] = min(house[i - 1][1], house[i - 1][2])+a
+    house[i][0] = min(house[i - 1][1], house[i - 1][2]) + a
     house[i][1] = min(house[i - 1][0], house[i - 1][2]) + b
     house[i][2] = min(house[i - 1][0], house[i - 1][1]) + c
 
