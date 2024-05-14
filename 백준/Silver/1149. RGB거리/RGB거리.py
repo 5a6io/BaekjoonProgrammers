b75380ed1fd3c6bf25f9e57c8348505f25184d4b
윤혜미 <78525791+YunHye-Mi@users.noreply.github.com>
2024-05-14 19:34:51 +0900

    [Silver I] Title: RGB거리, Time: 132 ms, Memory: 110272 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -0,0 +1,12 @@
+N = int(input())
+
+house = [[0, 0, 0] for i in range(N)]
+house[0] = list(map(int, input().split()))
+
+for i in range(1, N):
+    a, b, c = map(int, input().split())
+    house[i][0] = min(house[i - 1][1], house[i - 1][2])+a
+    house[i][1] = min(house[i - 1][0], house[i - 1][2]) + b
+    house[i][2] = min(house[i - 1][0], house[i - 1][1]) + c
+
+print(min(house[N-1]))
