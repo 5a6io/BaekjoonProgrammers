d8dc96b5ec5616c85c8024154cf109a12c88ca27
윤혜미 <78525791+YunHye-Mi@users.noreply.github.com>
2024-05-22 21:32:26 +0900

    [level 3] Title: 금과 은 운반하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/3/86053. 금과 은 운반하기/금과 은 운반하기.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <vector>
+#include <climits>
+
+using namespace std;
+
+long long solution(int a, int b, vector<int> g, vector<int> s, vector<int> w, vector<int> t) {
+    long long answer = LLONG_MAX;
+
+    long long start = 0;
+    long long end = 10e5 * 2 * 2 * 10e9; // 도시 수 * 재화 수 * 왕복 * 재화량
+
+    while (start <= end) {
+        long long mid = (start + end) / 2;
+
+        long long gold = 0;
+        long long silver = 0;
+        long long weight = 0;
+
+        for (int i = 0; i < g.size(); ++i) {
+            long long cnt = (mid + t[i]) / (t[i] * 2);
+            gold += min(g[i] * 1LL, w[i] * cnt * 1LL);
+            silver += min(s[i] * 1LL, w[i] * cnt * 1LL);
+            weight += min((g[i] + s[i]) * 1LL, w[i] * cnt * 1LL);
+        }
+
+        if (weight >= a + b && gold >= a && silver >= b) {
+            answer = min(answer, mid);
+            end = mid - 1;
+        } else {
+            start = mid + 1;
+        }
+    }
+
+    return answer;
+}
