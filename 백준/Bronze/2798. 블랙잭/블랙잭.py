71fd615a8446689f892077e3311342778804de31
Hyemi <78525791+5a6io@users.noreply.github.com>
2024-07-20 16:03:37 +0900

    [Bronze II] Title: 블랙잭, Time: 96 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -2,9 +2,9 @@ n, m = map(int, input().split())
 cards = list(map(int, input().split()))
 
 res = 0
-for i in range(n):
-    for j in range(n):
-        for k in range(n):
+for i in range(n - 2):
+    for j in range(i + 1, n - 1):
+        for k in range(j + 1, n):
             if i != j and j != k and i != k:
                 if res < cards[i]+cards[j]+cards[k] <= m:
                     res = cards[i]+cards[j]+cards[k]
