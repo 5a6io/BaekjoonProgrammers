ec9502b1139ffeaa182d8fe9d7a07c0902d0f616
Hyemi <78525791+5a6io@users.noreply.github.com>
2024-09-03 23:29:15 +0900

    [level 3] Title: 순위, Time: 6.46 ms, Memory: 11 MB -BaekjoonHub

---
+++ 프로그래머스/3/49191. 순위/순위.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+
+
+def solution(n, results):
+    answer = 0
+    win, lose = defaultdict(set), defaultdict(set)
+
+    for i, j in results:
+        win[i].add(j)
+        lose[j].add(i)
+
+    for i in range(1, n + 1):
+        for w in win[i]:
+            lose[w].update(lose[i])
+        for l in lose[i]:
+            win[l].update(win[i])
+
+    for i in range(1, n + 1):
+        if len(win[i]) + len(lose[i]) == n - 1:
+            answer += 1
+
+    return answer
