8cec091be8dc785362cd367727a44d6b47392534
Hyemi <78525791+5a6io@users.noreply.github.com>
2024-09-09 22:17:13 +0900

    [Bronze V] Title: 윤년, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.py
@@ -0,0 +1,10 @@
+def is_leap_year(y):
+    if y % 4 == 0 and (y % 100 != 0 or y % 400 == 0):
+        return True
+    else:
+        return False
+
+
+y = int(input())
+
+print(int(is_leap_year(y)))
