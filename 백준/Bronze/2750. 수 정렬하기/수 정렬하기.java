e5229440e09d720e1e5689cdfb7bde0a949d02fe
Hyemi <78525791+5a6io@users.noreply.github.com>
2024-09-28 00:27:38 +0900

    [Bronze II] Title: 수 정렬하기, Time: 276 ms, Memory: 22372 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main {
+
+    static int n;
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        int[] a = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++){
+                if (a[i] > a[j]){
+                    int temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
