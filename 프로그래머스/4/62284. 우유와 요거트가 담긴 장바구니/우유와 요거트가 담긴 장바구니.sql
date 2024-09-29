119df6a496bf3c3ec8f221f53248bedd23ede436
Hyemi <78525791+5a6io@users.noreply.github.com>
2024-09-29 15:47:53 +0900

    [level 4] Title: 우유와 요거트가 담긴 장바구니, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
@@ -0,0 +1,9 @@
+SELECT CART_ID
+FROM CART_PRODUCTS
+WHERE CART_ID IN (SELECT CART_ID
+                  FROM CART_PRODUCTS
+                  WHERE NAME = 'Milk')
+    AND CART_ID IN (SELECT CART_ID
+                    FROM CART_PRODUCTS
+                    WHERE NAME = 'Yogurt')
+GROUP BY CART_ID
