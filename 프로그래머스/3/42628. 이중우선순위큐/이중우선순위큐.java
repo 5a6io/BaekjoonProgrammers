5286b9fdc29cee10ec8b733fd96706b55a98ad86
Hyemi <78525791+5a6io@users.noreply.github.com>
2024-10-10 19:45:07 +0900

    [level 3] Title: 이중우선순위큐, Time: 68.05 ms, Memory: 130 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -4,28 +4,33 @@ class Solution {
     public int[] solution(String[] operations) {
         int[] answer = new int[2];
         
-        List<Integer> dq = new ArrayList<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(); // 최소 힙
+        PriorityQueue<Integer> rpq = new PriorityQueue<>(Collections.reverseOrder()); // 최대 힙
         
         for(int i = 0; i < operations.length; i++){
             String[] s = operations[i].split(" ");
             if(s[0].equals("I")){
-                dq.add(Integer.parseInt(s[1]));
+                pq.add(Integer.parseInt(s[1]));
+                rpq.add(Integer.parseInt(s[1]));
             }
-            else if(!dq.isEmpty() && s[0].equals("D")){
-                if(s[1].equals("1"))
-                    dq.remove(0);
-                else
-                    dq.remove(dq.size() - 1);
+            else if(!pq.isEmpty() && !rpq.isEmpty() && s[0].equals("D")){
+                if(s[1].equals("1")){
+                    int max = rpq.poll();
+                    pq.remove(max);
+                }
+                else{
+                    int min = pq.poll();
+                    rpq.remove(min);
+                }
             }
-            dq.sort((o1, o2) -> (o2 - o1));
         }
         
-        if(dq.isEmpty()){
+        if(pq.isEmpty() && rpq.isEmpty()){
             answer[0] = 0;
             answer[1] = 0;
         }else{
-            answer[0] = dq.get(0);
-            answer[1] = dq.get(dq.size() - 1);
+            answer[0] = rpq.peek();
+            answer[1] = pq.peek();
         }
         
         return answer;
