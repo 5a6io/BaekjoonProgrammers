4aab2cd6003da74af32ee4179ef2d0c0092d74e0
Hyemi <78525791+5a6io@users.noreply.github.com>
2024-10-14 18:28:32 +0900

    [level 3] Title: 정수 삼각형, Time: 17.99 ms, Memory: 59.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[][] triangle) {
+        int answer = 0;
+        int n = triangle.length;
+        int[][] dp = new int[n][n];
+        dp[0][0] = triangle[0][0];
+        
+        for(int i = 0; i < n - 1; i++){
+            for(int j = 0; j < triangle[i].length; j++){
+                dp[i + 1][j] = Math.max(dp[i][j] + triangle[i + 1][j], dp[i + 1][j]);
+                dp[i + 1][j + 1] = Math.max(dp[i][j] + triangle[i + 1][j + 1], dp[i + 1][j +  1]);
+            }
+        }
+        
+        return answer = Arrays.stream(dp[n - 1]).max().orElse(0);
+    }
+}
