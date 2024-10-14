cd6230fa1b00ac49eda62ba3c6b6b2e2f2d014a1
Hyemi <78525791+5a6io@users.noreply.github.com>
2024-10-14 21:41:22 +0900

    [level 3] Title: 등굣길, Time: 0.98 ms, Memory: 51.9 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -10,12 +10,13 @@ class Solution {
         dp[1][1] = 1;
         for(int i = 1; i < n + 1; i++){
             for(int j = 1; j < m + 1; j++){
-                if(dp[i][j] == -1)
-                    continue;
-                if(dp[i - 1][j] > 0)
-                    dp[i][j] = (dp[i][j] + dp[i - 1][j]) % 1000000007;
-                if(dp[i][j - 1] > 0)
-                    dp[i][j] = (dp[i][j] + dp[i][j - 1]) % 1000000007;
+                if(dp[i][j] != -1){
+                    if(dp[i - 1][j] > 0)
+                        dp[i][j] = (dp[i][j] + dp[i - 1][j]) % 1000000007;
+                    if(dp[i][j - 1] > 0)
+                        dp[i][j] = (dp[i][j] + dp[i][j - 1]) % 1000000007;
+                }
+                
             }
         }
         
