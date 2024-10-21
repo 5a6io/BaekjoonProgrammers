038bc5f99efd3cc7c5559c7ad50f4871832524bb
Hyemi <78525791+5a6io@users.noreply.github.com>
2024-10-21 09:43:51 +0900

    [level 2] Title: 괄호 회전하기, Time: 91.27 ms, Memory: 96 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -3,18 +3,18 @@ import java.util.*;
 class Solution {
     public int solution(String s) {
         int answer = 0;
-        char[] c = s.toCharArray();
-        int n = c.length;
+        int n = s.length();
+        
         for(int i = 0; i < n; i++){
             Stack<Character> st = new Stack<>();
             for(int j = i; j < i + n; j++){
                 int idx = j % n;
-                if(st.isEmpty()) st.push(c[idx]);
+                if(st.isEmpty()) st.push(s.charAt(idx));
                 else{
-                    if(st.peek() == '(' && c[idx] == ')') st.pop();
-                    else if(st.peek() == '[' && c[idx] == ']') st.pop();
-                    else if(st.peek() == '{' && c[idx] == '}') st.pop();
-                    else st.push(c[idx]);
+                    if(st.peek() == '(' && s.charAt(idx) == ')') st.pop();
+                    else if(st.peek() == '[' && s.charAt(idx) == ']') st.pop();
+                    else if(st.peek() == '{' && s.charAt(idx) == '}') st.pop();
+                    else st.push(s.charAt(idx));
                 }
             }
             if(st.isEmpty())
