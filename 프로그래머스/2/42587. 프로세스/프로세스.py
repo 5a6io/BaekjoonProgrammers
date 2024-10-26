c085194a014db72ebfff6ec30d4c396e815b4d58
Hyemi <78525791+5a6io@users.noreply.github.com>
2024-10-26 12:58:44 +0900

    [level 2] Title: 프로세스, Time: 0.81 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,12 +1,12 @@
 def solution(priorities, location):
-    process=[(i, p) for i, p in enumerate(priorities)]
+    process = [(x, y) for x, y in zip(range(len(priorities)), priorities)]
     answer = 0
     
     while True:
-        cur = process.pop(0)
-        if any(cur[1] < p[1] for p in process):
-            process.append(cur)
+        now = process.pop(0)
+        if any(now[1] < p[1] for p in process):
+            process.append(now)
         else:
             answer += 1
-            if cur[0] == location:
+            if location == now[0]:
                 return answer
