675f7b9ae08b52e56a7820911796c50e6f0dbc21
Hyemi <78525791+5a6io@users.noreply.github.com>
2024-10-26 15:57:47 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 1.40 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/README.md
+++ 프로그래머스/3/42627. 디스크 컨트롤러/README.md
@@ -1,10 +1,10 @@
 # [level 3] 디스크 컨트롤러 - 42627 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42627?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42627?itm_content=course14743) 
 
 ### 성능 요약
 
-메모리: 78.3 MB, 시간: 4.46 ms
+메모리: 10.1 MB, 시간: 1.40 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 10일 00:03:37
+2024년 10월 26일 15:57:44
 
 ### 문제 설명
 
--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,21 +1,16 @@
-import heapq
-
 def solution(jobs):
-    answer, now, i = 0, 0, 0
-    start = -1
-    work = []
-
-    while i  < len(jobs):
-        # 현 시점에서 처리할 수 있는 작업들 큐에서 대기(요청 시간이 같으면 수행시간이 작은 것부터 실행해야 하므로 최소힙 사용)
-        for j in jobs:
-            if start < j[0] <= now:
-                heapq.heappush(work, (j[1], j[0]))
-        if work:
-            cur = heapq.heappop(work)
-            start = now
-            now += cur[0]
-            answer += (now - cur[1])
-            i += 1
-        else:
-            now += 1
-    return answer // len(jobs)
+    answer, time = 0, 0
+    jobs.sort(key = lambda x : x[1])
+    n = len(jobs)
+    
+    while len(jobs) != 0:
+        for i in range(len(jobs)):
+            if jobs[i][0] <= time:
+                time += jobs[i][1]
+                answer += time - jobs[i][0]
+                jobs.pop(i)
+                break
+            if len(jobs) - 1 == i:
+                time += 1
+    
+    return answer // n
