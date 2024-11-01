faf025f57aafcb95d65d0e84fb2aa1011dac75b0
Hyemi <78525791+5a6io@users.noreply.github.com>
2024-11-01 20:06:31 +0900

    [Silver III] Title: 주유소, Time: 112 ms, Memory: 46228 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -8,7 +8,10 @@ prices = list(map(int, input().split()))
 dp = [0] * (n - 1)
 dp[0] = lengths[0] * prices[0]
 
+minCost = prices[0]
 for i in range(1, n - 1):
-    dp[i] = dp[i - 1] + min(lengths[i] * prices[i], lengths[i] * prices[i - 1])
+    if minCost > prices[i]:
+        minCost = prices[i]
+    dp[i] = dp[i - 1] + minCost * lengths[i]
 
 print(dp[n - 2])
