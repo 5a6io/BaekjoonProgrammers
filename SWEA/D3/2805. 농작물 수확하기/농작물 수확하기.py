41304d6a45605d3abeeadabaa7411e1a1a09c9c4
Hyemi <78525791+5a6io@users.noreply.github.com>
2024-11-11 23:21:20 +0900

    [D3] Title: 농작물 수확하기, Time: 174 ms, Memory: 50,364 KB -BaekjoonHub

--- SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.py
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.py
@@ -10,11 +10,9 @@ for i in range(1, t + 1):
         res = 0
         mid = n//2
         for j in range(mid):
-            res += sum(farm[j][mid - j:mid])
-            res += sum(farm[j][mid: mid + j + 1])
+            res += sum(farm[j][mid - j:mid + j + 1])
 
         for j in range(mid, n):
-            res += sum(farm[j][j - mid:mid])
-            res += sum(farm[j][mid:n - j + mid])
+            res += sum(farm[j][j - mid:n - j + mid])
 
         print(f"#{i} {res}")
