10efbcd752ce51914b6d1e3a6a5be3169b524f68
Hyemi <78525791+5a6io@users.noreply.github.com>
2024-11-13 19:19:20 +0900

    [D3] Title: [S/W 문제해결 기본] 7일차 - 암호생성기, Time: 119 ms, Memory: 48,136 KB -BaekjoonHub

--- SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.py
+++ SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.py
@@ -1,15 +1,12 @@
-from collections import deque
-
 for i in range(10):
     t = int(input())
-    codes = deque(list(map(int, input().split())))
+    codes = list(map(int, input().split()))
 
     sub = 1
     while True:
         if sub > 5:
             sub = 1
-        cur = codes.popleft()
-        cur -= sub
+        cur = codes.pop(0) - sub
         if cur <= 0:
             codes.append(0)
             break
