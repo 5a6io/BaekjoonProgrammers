107b28bfbc4a60f58338c3bd30d0dcc84ea843cf
Hyemi <78525791+5a6io@users.noreply.github.com>
2024-11-16 17:49:25 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - 회문1, Time: 141 ms, Memory: 45,840 KB -BaekjoonHub

--- SWEA/D3/1215. ［S／W 문제해결 기본］ 3일차 － 회문1/［S／W 문제해결 기본］ 3일차 － 회문1.py
+++ SWEA/D3/1215. ［S／W 문제해결 기본］ 3일차 － 회문1/［S／W 문제해결 기본］ 3일차 － 회문1.py
@@ -1,3 +1,22 @@
+def palindromic(y, x, s):
+    if len(s) == l:
+        if s == "".join(s[-1::-1]):
+            return 1
+        else:
+            return 0
+
+    return palindromic(y, x + 1, s + board[y][x])
+
+def t_palindromic(y, x, s):
+    if len(s) == l:
+        if s == "".join(s[-1::-1]):
+            return 1
+        else:
+            return 0
+
+    return t_palindromic(y, x + 1, s + t_board[y][x])
+
+
 for tc in range(1, 11):
     l = int(input())
     board = [list(input()) for _ in range(8)]
@@ -5,12 +24,8 @@ for tc in range(1, 11):
 
     res = 0
     for i in range(8):
-        for j in range(8-l+1):
-            s = board[i][j:j+l]
-            if s == s[::-1]:
-                res += 1
-            t_s = t_board[i][j:j+l]
-            if t_s == t_s[::-1]:
-                res += 1
+        for j in range(8-l + 1):
+            res += palindromic(i, j, "")
+            res += t_palindromic(i, j, "")
 
     print(f"#{tc} {res}")
