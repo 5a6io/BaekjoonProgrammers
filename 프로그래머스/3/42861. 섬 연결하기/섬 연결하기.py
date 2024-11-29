86f951c0f77896b7c496d3612d995d5776239c79
Hyemi <78525791+5a6io@users.noreply.github.com>
2024-11-29 17:44:08 +0900

    [level 3] Title: 섬 연결하기, Time: 0.07 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -0,0 +1,12 @@
+def solution(n, costs):
+    answer = 0
+    costs.sort(key= lambda x:x[2])
+    tree = set([costs[0][0]])
+    while len(tree) < n:
+        for c in costs:
+            if len(set(c[:2]) & tree) == 1:
+                tree.update(c[:2])
+                answer += c[2]
+                break
+
+    return answer
