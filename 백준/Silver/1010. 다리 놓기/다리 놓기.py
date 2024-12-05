e29b70dcaecb972ae43b8dc5e7c49040f22da210
Hyemi <78525791+5a6io@users.noreply.github.com>
2024-12-06 00:25:29 +0900

    [Silver V] Title: 다리 놓기, Time: 92 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.py
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -6,14 +6,11 @@ ts = int(input())
 res = 0
 for _ in range(ts):
     n, m = map(int, input().split())
-    dp = [0] * (m+1)
+    dp = [0] * (n+1)
     dp[0] = 1
     dp[1] = m
 
-    for i in range(2, m//2+1):
+    for i in range(2, n+1):
         dp[i] = dp[i - 1]*(m-i+1)//i
 
-    if n > m//2:
-        print(dp[m-n])
-    else:
-        print(dp[n])
+    print(dp[n])
