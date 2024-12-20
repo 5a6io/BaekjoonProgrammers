ce24024b709e609202cb3af020cb9a3ea7341def
Hyemi <78525791+5a6io@users.noreply.github.com>
2024-12-20 17:10:23 +0900

    [Silver IV] Title: ATM, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -3,11 +3,11 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-p = list(zip(range(1, n+1), list(map(int, input().split()))))
-p.sort(key= lambda x: x[1])
+p = list(map(int, input().split()))
+p.sort()
 
-answer = [p[0][1]]
+answer = [p[0]]
 for i in range(1, n):
-    answer.append(answer[i-1] + p[i][1])
+    answer.append(answer[i-1] + p[i])
 
 print(sum(answer))
