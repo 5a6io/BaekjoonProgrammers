5548cd4c8642937870a2807fdeade0911eaa89ac
Hyemi <78525791+5a6io@users.noreply.github.com>
2025-01-27 15:30:47 +0900

    [level 2] Title: 큰 수 만들기, Time: 129.68 ms, Memory: 14.7 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -1,6 +1,5 @@
 def solution(number, k):
     answer = ''
-    
     num = []
     for n in number:
         while len(num) > 0 and num[-1] < n and k > 0:
@@ -8,8 +7,4 @@ def solution(number, k):
             k -= 1
         num.append(n)
     
-    while k > 0 and num[-2] > num[-1]:
-        num.pop()
-        k -= 1
-    
-    return answer.join(num)
+    return answer.join(num[:len(number)-k])
