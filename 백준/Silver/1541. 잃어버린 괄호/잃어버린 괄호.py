13e755be31ccf4ccf7574cd17b697a0cc1bda598
Hyemi <78525791+5a6io@users.noreply.github.com>
2025-01-29 15:33:48 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -1,11 +1,12 @@
 import sys
-input = sys.stdin.readline
-S = input().strip().split('-')
-result = 0
-for i in S[0].split('+'):
-    result += int(i)
-for i in S[1:]:
-    for j in i.split('+'):
-        result -= int(j)
 
-print(result)
+input = sys.stdin.readline
+
+S = input().strip().split('-')
+
+res = sum(list(map(int, S[0].split('+'))))
+for i in range(1, len(S)):
+    tmp = list(map(int, S[i].split('+')))
+    res -= sum(tmp)
+
+print(res)
