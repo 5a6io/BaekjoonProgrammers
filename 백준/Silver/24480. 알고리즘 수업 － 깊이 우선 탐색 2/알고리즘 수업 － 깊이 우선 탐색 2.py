fc5a67765213109ff0dbad1f3123b974a342558d
Hyemi <78525791+5a6io@users.noreply.github.com>
2025-02-04 16:48:14 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 552 ms, Memory: 69288 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.py
@@ -0,0 +1,32 @@
+import sys
+
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6) # recursion limit 늘리기
+
+N, M, R = map(int, input().split())
+graph = [[] for _ in range(N+1)]
+for _ in range(M):
+    U, V = map(int, input().split())
+    graph[U].append(V)
+    graph[V].append(U)
+
+for i in range(1, N+1):
+    graph[i].sort(reverse=True)
+
+visited = [False] * (N+1)
+cnt = 1
+def dfs(u):
+    global cnt
+    visited[u] = cnt
+    cnt += 1
+    for v in graph[u]:
+        if not visited[v]:
+            visited[v] = True
+            dfs(v)
+
+dfs(R)
+for i in range(1, N+1):
+    if not visited[i]:
+        print(0)
+    else:
+        print(visited[i])
