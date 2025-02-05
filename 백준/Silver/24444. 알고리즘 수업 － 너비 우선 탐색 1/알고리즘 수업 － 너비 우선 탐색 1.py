67d914966885f2742fd1d027915f8ffb8fdd3515
Hyemi <78525791+5a6io@users.noreply.github.com>
2025-02-05 14:34:23 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 404 ms, Memory: 133596 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
@@ -0,0 +1,35 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+N, M, R = map(int, input().split())
+graph = [[] for _ in range(N+1)]
+visited = [False] * (N+1)
+for _ in range(M):
+    U, V = map(int, input().split())
+    graph[U].append(V)
+    graph[V].append(U)
+
+for i in range(1, N+1):
+    graph[i].sort()
+
+def bfs():
+    q = deque()
+    q.append(R)
+    visited[R] = 1
+    cnt = 2
+    while q:
+        u = q.popleft()
+        for v in graph[u]:
+            if not visited[v]:
+                q.append(v)
+                visited[v] = cnt
+                cnt += 1
+
+bfs()
+for i in range(1, N+1):
+    if not visited[i]:
+        print(0)
+    else:
+        print(visited[i])
