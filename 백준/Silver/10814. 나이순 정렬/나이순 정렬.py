414cade70b636e5ada48e8b63039956882d3d6bb
Hyemi <78525791+5a6io@users.noreply.github.com>
2025-02-13 16:05:22 +0900

    나이순 정렬.py 업데이트

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -6,7 +6,7 @@ N = int(input())
 M = []
 for _ in range(N):
     age, name = input().strip().split()
-    M.append([int(age), name])
+    M.append([int(age), name]) # 나이를 정수형으로 바꾸지 않고 넣으면 정렬 부분에서 틀림
 
 for m in sorted(M, key=lambda x: x[0]):
     print(m[0], m[1])
