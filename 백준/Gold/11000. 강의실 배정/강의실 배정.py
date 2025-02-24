67160f4a10fdc5ab4e4d90cfe39bd882b2545c04
Hyemi <78525791+5a6io@users.noreply.github.com>
2025-02-24 17:08:39 +0900

    [Gold V] Title: 강의실 배정, Time: 384 ms, Memory: 88836 KB -BaekjoonHub

---
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -0,0 +1,16 @@
+import sys
+import heapq
+
+input = sys.stdin.readline
+
+N = int(input())
+rooms = [list(map(int, input().split())) for _ in range(N)]
+rooms.sort(key=lambda x: (x[0], x[1]))
+
+q = [rooms[0][1]]
+for i in range(1, N):
+    if q[0] <= rooms[i][0]:
+        heapq.heappop(q)
+    heapq.heappush(q, rooms[i][1])
+
+print(len(q))
