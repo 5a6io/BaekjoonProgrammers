f911f21bfc2973d7cdbe787e49ec52a4f4034a19
Hyemi <78525791+5a6io@users.noreply.github.com>
2025-03-01 15:18:36 +0900

    [Gold V] Title: 탑, Time: 560 ms, Memory: 115756 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.py
+++ 백준/Gold/2493. 탑/탑.py
@@ -3,14 +3,17 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-res = [0] * N
 top = list(map(int, input().split()))
+res = [0] * N
 temp = []
 for i in range(N):
-    while temp and temp[-1][1] <= top[i]:
-        temp.pop()
-    if temp:
-        res[i] = temp[-1][0]
-    temp.append((i+1, top[i],))
+    while temp:
+        if temp[-1][1] > top[i]:
+            res[i] = temp[-1][0]
+            break
+        else:
+            temp.pop()
+
+    temp.append((i+1, top[i]))
 
 print(*res)
