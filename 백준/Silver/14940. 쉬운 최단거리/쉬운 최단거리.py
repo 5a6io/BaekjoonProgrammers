296a3204380b15d1bb4adf1ff465af6f61319515
Hyemi <78525791+5a6io@users.noreply.github.com>
2025-03-10 17:37:26 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 508 ms, Memory: 41932 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
@@ -6,25 +6,30 @@ input = sys.stdin.readline
 n, m = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(n)]
 visited = [[0 for _ in range(m)] for _ in range(n)]
+dy = [1, 0, 0, -1]
+dx = [0, 1, -1, 0]
+
 q = deque()
+
+def bfs(y, x):
+    q.append((y, x))
+    board[y][x] = 0
+    
+    while q:
+        y, x = q.popleft()
+
+        for i in range(4):
+            ny, nx = y + dy[i], x + dx[i]
+
+            if 0 <= ny < n and 0<= nx < m and not visited[ny][nx] and board[ny][nx] == 1:
+                visited[ny][nx] = visited[y][x] + 1
+                q.append((ny, nx))
+
 for y in range(n):
     for x in range(m):
         if board[y][x] == 2:
-            q.append((y, x))
-            board[y][x] = 0
+            bfs(y, x)
             break
-
-dy = [1, 0, 0, -1]
-dx = [0, 1, -1, 0]
-while q:
-    y, x = q.popleft()
-
-    for i in range(4):
-        ny, nx = y + dy[i], x + dx[i]
-
-        if 0 <= ny < n and 0<= nx < m and not visited[ny][nx] and board[ny][nx] == 1:
-            visited[ny][nx] = visited[y][x] + 1
-            q.append((ny, nx))
 
 for y in range(n):
     for x in range(m):
