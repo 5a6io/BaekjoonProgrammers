21221117ebd40e1e184220c0fb0d7f304e56422b
Hyemi <78525791+5a6io@users.noreply.github.com>
2025-03-19 18:14:41 +0900

    [Gold V] Title: 트리, Time: 64 ms, Memory: 35084 KB -BaekjoonHub

---
+++ 백준/Gold/1068. 트리/트리.py
@@ -0,0 +1,39 @@
+import sys
+from collections import defaultdict
+sys.setrecursionlimit(1000000)
+
+input = sys.stdin.readline
+
+N = int(input())
+nodes = list(map(int, input().split()))
+remove = int(input())
+tree = defaultdict(list)
+visited = [False] * N
+root = 0
+
+def dfs(r):
+    visited[r] = True
+    for n in tree[r]:
+        if not tree[n]:
+            visited[n] = True
+            tree.pop(n)
+        else:
+            dfs(n)
+    tree.pop(r)
+
+for v in range(N):
+    if nodes[v] == -1:
+        continue
+    tree[nodes[v]].append(v)
+
+dfs(remove)
+res = 0
+
+for n in range(N):
+    if not visited[n]:
+        if remove in tree[n] and len(tree[n])-1 == 0:
+            res +=1
+        if not tree[n]:
+            res += 1
+
+print(res)
