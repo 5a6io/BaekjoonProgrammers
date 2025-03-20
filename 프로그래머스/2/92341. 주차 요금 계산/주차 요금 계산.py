1981a9e0c590f8e3d7bdcd465da92da6100a901e
Hyemi <78525791+5a6io@users.noreply.github.com>
2025-03-20 19:10:08 +0900

    [level 2] Title: 주차 요금 계산, Time: 2.57 ms, Memory: 9.72 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.py
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.py
@@ -14,18 +14,15 @@ def solution(fees, records):
         if len(cars[car]) % 2 != 0:
             cars[car].append("23:59")
 
-        cost = 0
         m = 0
         for i in range(0, len(cars[car]), 2):
             ih, im = cars[car][i].split(":")
             oh, om = cars[car][i + 1].split(":")
             m += (int(oh) - int(ih)) * 60 + int(om) - int(im)
         if m <= fees[0]:
-            cost += fees[1]
+            fee.append((car, fees[1]))
         else:
-            cost += fees[1]
-            cost += math.ceil((m - fees[0])/fees[2]) * fees[3]
-        fee.append((car, cost))
+            fee.append((car, fees[1] + math.ceil((m - fees[0])/fees[2]) * fees[3]))
 
     for f in sorted(fee, key=lambda x: x[0]):
         answer.append(f[1])
