73b8a811e7ec0616991db0b108f86b029601bb7b
Hyemi <78525791+5a6io@users.noreply.github.com>
2025-04-04 15:43:08 +0900

    [level 2] Title: 프로세스, Time: 0.37 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,12 +1,20 @@
+from collections import deque
+
 def solution(priorities, location):
-    process = [(x, y) for x, y in zip(range(len(priorities)), priorities)]
-    answer = 0
+    answer = 1
+    q = list(zip(priorities, range(len(priorities))))
     
-    while True:
-        now = process.pop(0)
-        if any(now[1] < p[1] for p in process):
-            process.append(now)
-        else:
+    mx = max(q)[0]
+    
+    while q:
+        p, i = q.pop(0)
+        
+        if p == mx:
+            if i == location:
+                break
             answer += 1
-            if location == now[0]:
-                return answer
+            mx = max(q)[0]
+        else:
+            q.append((p, i))
+    
+    return answer
