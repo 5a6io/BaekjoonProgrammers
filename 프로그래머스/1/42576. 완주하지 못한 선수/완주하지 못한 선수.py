5ac4d7d639b9d545e35b43f58531238b2babcaf8
Hyemi <78525791+5a6io@users.noreply.github.com>
2025-07-07 19:19:56 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 65.81 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,15 +1,4 @@
+from collections import Counter
+
 def solution(participant, completion):    
-    dic = {}
-    for p in participant:
-        if p not in dic:
-            dic[p] = 1
-        else:
-            dic[p] += 1
-    
-    for c in completion:
-        if dic[c] > 1:
-            dic[c] -= 1
-        else:
-            dic.pop(c)
-    
-    return list(dic.keys())[0]
+    return list(Counter(participant) - Counter(completion))[0]
