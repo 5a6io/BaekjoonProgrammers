0d3292b5949410e98d1f30ab39a6b3f4402acff4
Hyemi <78525791+5a6io@users.noreply.github.com>
2025-07-10 13:08:56 +0900

    [level 2] Title: 전화번호 목록, Time: 108.76 ms, Memory: 30 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,7 +1,8 @@
 def solution(phone_book):
     phone_book.sort()
+    
     for i in range(len(phone_book)-1):
-        phone = phone_book[i+1]
-        if phone.startswith(phone_book[i]):
+        if phone_book[i] in phone_book[i+1][:len(phone_book[i])]:
             return False
+    
     return True
