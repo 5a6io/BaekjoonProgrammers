976d3d27261128097e6236cdd8b732d1ea472a6f
Hyemi <78525791+5a6io@users.noreply.github.com>
2025-07-15 19:04:29 +0900

    [level 1] Title: 체육복, Time: 0.03 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.03 ms
+메모리: 9.19 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 28일 14:42:44
+2025년 07월 15일 19:04:25
 
 ### 문제 설명
 
--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,20 +1,13 @@
+from collections import deque
+
 def solution(n, lost, reserve):
-    answer = 0
-    cnt = [0] * (n + 1)
-    for i in range(1, n + 1):
-        if i in lost:
-            cnt[i] -= 1
-        if i in reserve:
-            cnt[i] += 1
-        
-    for i in range(1, n + 1):
-        if cnt[i] == 1 and i - 1 != 0 and cnt[i - 1] < 0:
-            cnt[i] = cnt[i-1] = 0
-        if cnt[i] == 1 and i + 1 != n + 1 and cnt[i + 1] < 0:
-            cnt[i] = cnt[i+1] = 0
-            
-    for i in range(1, n + 1):
-        if cnt[i] > -1:
-            answer += 1
+    ls = set(lost) - set(reserve)
+    rs = set(reserve) - set(lost)
     
-    return answer
+    for r in rs:
+        if r-1 in ls:
+            ls.remove(r-1)
+        elif r+1 in ls:
+            ls.remove(r+1)
+    
+    return n - len(ls)
