aa9e178954855685ed342e7da1b90076f92cc97c
Hyemi <78525791+5a6io@users.noreply.github.com>
2025-07-16 13:08:51 +0900

    [level 1] Title: 최소직사각형, Time: 3.29 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -1,12 +1,3 @@
 def solution(sizes):
-    answer = 0
-    for size in sizes:
-        size.sort(reverse=True)
-    
-    sizes.sort(key=lambda x: -x[0])
-    
-    w = sizes[0][0]
-    for _, h in sizes:
-        answer = max(answer, w*h)
-               
+    answer = max(max(s) for s in sizes) * max(min(s) for s in sizes)
     return answer
