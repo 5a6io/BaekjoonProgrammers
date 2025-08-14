164aa26f93149641ed0abc9e0ee2ecd7e6cffa91
Hyemi <78525791+5a6io@users.noreply.github.com>
2025-08-14 18:36:36 +0900

    [Gold IV] Title: 타임머신, Time: 184 ms, Memory: 111428 KB -BaekjoonHub

---
+++ 백준/Gold/11657. 타임머신/타임머신.py
@@ -0,0 +1,24 @@
+import sys
+
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+INF = 1e9
+edges = [list(map(int, input().split())) for _ in range(M)]
+dist = [0 if v == 0 else INF for v in range(N)]
+
+def bellmanford(v):
+    for v in range(N):
+        for a, b, c in edges:
+            if dist[a-1] != INF and dist[b-1] > dist[a-1] + c:
+                dist[b-1] = dist[a-1] + c
+                if v == N-1:
+                    return True
+
+    return False
+
+if bellmanford(0):
+    print(-1)
+else:
+    for v in range(1, N):
+        print(dist[v] if dist[v] != INF else -1)
