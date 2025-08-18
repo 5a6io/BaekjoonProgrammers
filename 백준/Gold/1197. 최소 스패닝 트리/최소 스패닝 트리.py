fe3023913fd1f088df0913152800cb48a7e32340
Hyemi <78525791+5a6io@users.noreply.github.com>
2025-08-18 16:19:37 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 380 ms, Memory: 129144 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,30 +1,30 @@
 import sys
+from heapq import heappop, heappush
 
 input = sys.stdin.readline
 INF = 1e9
 
-V, E = map(int, input().split())
-graph = [[] for _ in range(V)]
+n, m = map(int, input().split())
+edges = [[] for _ in range(n)]
+visited = [0] * n
 
-for _ in range(E):
+for _ in range(m):
     a, b, c = map(int, input().split())
-    graph[a-1].append((b-1, c))
-    graph[b-1].append((a-1, c))
+    edges[a - 1].append((c, b - 1))
+    edges[b - 1].append((c, a - 1))
 
-visited = [0] * V
-dist = [INF] * V
-dist[0] = 0
+ans = 0
+q = [(0, 0)]
 
-for _ in range(V):
-    mn = INF
-    b = -1
-    for v in range(V):
-        if not visited[v] and dist[v] < mn:
-            mn = dist[v]
-            b = v
-    visited[b] = 1
-    for u, c in graph[b]:
-        if not visited[u]:
-            dist[u] = min(dist[u], c)
+while q:
+    c, v = heappop(q)
 
-print(sum(dist))
+    if not visited[v]:
+        visited[v] = 1
+        ans += c
+
+        for c, u in edges[v]:
+            if not visited[u]:
+                heappush(q, (c, u))
+
+print(ans)
