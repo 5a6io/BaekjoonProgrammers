d3026dc720abda403ce7d9721e37b9036897e768
Hyemi <78525791+5a6io@users.noreply.github.com>
2025-09-10 12:18:01 +0900

    [Gold IV] Title: 운동, Time: 2200 ms, Memory: 112724 KB -BaekjoonHub

---
+++ 백준/Gold/1956. 운동/운동.py
@@ -0,0 +1,25 @@
+import sys
+from heapq import heappush, heappop
+
+input = sys.stdin.readline
+INF = sys.maxsize
+
+V, E = map(int, input().split())
+board = [[INF if i != j else 0 for j in range(V)] for i in range(V)]
+
+for _ in range(E):
+    a, b, c = map(int, input().split())
+    board[a-1][b-1] = c
+
+for k in range(V):
+    for i in range(V):
+        for j in range(V):
+            board[i][j] = min(board[i][j], board[i][k] + board[k][j])
+
+res = INF
+for v in range(V):
+    for u in range(V):
+        if u!= v:
+            res = min(res, board[v][u] + board[u][v])
+        
+print(-1) if res >= INF else print(res)
