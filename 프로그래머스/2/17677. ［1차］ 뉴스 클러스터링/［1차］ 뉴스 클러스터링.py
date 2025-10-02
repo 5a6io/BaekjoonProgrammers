ca330811e1267fb95a47d3b7da71cca52480604e
Hyemi <78525791+5a6io@users.noreply.github.com>
2025-10-02 16:44:26 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 1.36 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
@@ -1,21 +1,12 @@
+from collections import Counter
+
 def solution(str1, str2):
     _str1 = [str1[i:i+2].lower() for i in range(len(str1)-1) if str1[i:i+2].isalpha()]
     _str2 = [str2[i:i+2].lower() for i in range(len(str2)-1) if str2[i:i+2].isalpha()]
-    if len(_str1) == 0 and len(_str2) == 0:
+    if not _str1 and not _str2:
         return 65536
-    union = len(_str1) + len(_str2)
-    inter = []
-    if len(_str1) > len(_str2):
-        _str = _str2.copy()
-        for s in _str1:
-            if s in _str:
-                _str.remove(s)
-                inter.append(s)
-    else:
-        _str = _str1.copy()
-        for s in _str2:
-            if s in _str:
-                _str.remove(s)
-                inter.append(s)
-    answer = int(len(inter)/(union-len(inter))*65536)
+    union = sum((Counter(_str1)|Counter(_str2)).values())
+    inter = sum((Counter(_str1)&Counter(_str2)).values())
+    
+    answer = int(inter/union*65536)
     return answer
