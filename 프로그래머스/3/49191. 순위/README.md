46e95add31a3f47006442fcfdb785f270832fdb0
Hyemi <78525791+5a6io@users.noreply.github.com>
2025-10-21 21:34:52 +0900

    [level 3] Title: 순위, Time: 361.03 ms, Memory: 9.7 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/README.md
+++ 프로그래머스/3/49191. 순위/README.md
@@ -1,10 +1,10 @@
 # [level 3] 순위 - 49191 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49191?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49191) 
 
 ### 성능 요약
 
-메모리: 84.1 MB, 시간: 9.84 ms
+메모리: 9.7 MB, 시간: 361.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 18일 23:20:44
+2025년 10월 21일 21:34:49
 
 ### 문제 설명
 
--- 프로그래머스/3/49191. 순위/순위.py
+++ 프로그래머스/3/49191. 순위/순위.py
@@ -1,22 +1,31 @@
-from collections import defaultdict
-
-
 def solution(n, results):
     answer = 0
-    win, lose = defaultdict(set), defaultdict(set)
-
-    for i, j in results:
-        win[i].add(j)
-        lose[j].add(i)
-
-    for i in range(1, n + 1):
-        for w in win[i]:
-            lose[w].update(lose[i])
-        for l in lose[i]:
-            win[l].update(win[i])
-
-    for i in range(1, n + 1):
-        if len(win[i]) + len(lose[i]) == n - 1:
+    INF = 1e9
+    wins = [[0 if a == b else INF for b in range(n)] for a in range(n)] 
+    loses = [[0 if a == b else INF for a in range(n)] for b in range(n)]
+    
+    for a, b in results:
+        wins[a-1][b-1] = 1
+        loses[b-1][a-1] = 1
+    
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                if wins[i][j] > wins[i][k] + wins[k][j]:
+                    wins[i][j] = wins[i][k] + wins[k][j]
+                if loses[j][i] > loses[j][k] + loses[k][i]:
+                    loses[j][i] = loses[j][k] + loses[k][i]
+    
+    for a in range(n):
+        win = 0
+        lose = 0
+        for b in range(n):
+            if a != b:
+                if wins[a][b] != INF:
+                    win += 1
+                if loses[a][b] != INF:
+                    lose += 1
+        if win + lose == n-1:
             answer += 1
-
+                
     return answer
