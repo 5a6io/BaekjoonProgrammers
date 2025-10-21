59ef69ccec943b28046f0382820eda8f6e76df1b
Hyemi <78525791+5a6io@users.noreply.github.com>
2025-10-21 21:54:50 +0900

    [level 3] Title: 순위, Time: 8.61 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.py
+++ 프로그래머스/3/49191. 순위/순위.py
@@ -1,31 +1,18 @@
+from collections import defaultdict
+
 def solution(n, results):
     answer = 0
-    INF = 1e9
-    wins = [[0 if a == b else INF for b in range(n)] for a in range(n)] 
-    loses = [[0 if a == b else INF for a in range(n)] for b in range(n)]
+    win, lose = defaultdict(set), defaultdict(set)
     
     for a, b in results:
-        wins[a-1][b-1] = 1
-        loses[b-1][a-1] = 1
+        win[a].add(b)
+        lose[b].add(a)
+        
+    for i in range(1, n+1):
+        for w in lose[i]: win[w].update(win[i])
+        for l in win[i]: lose[l].update(lose[i])
     
-    for k in range(n):
-        for i in range(n):
-            for j in range(n):
-                if wins[i][j] > wins[i][k] + wins[k][j]:
-                    wins[i][j] = wins[i][k] + wins[k][j]
-                if loses[j][i] > loses[j][k] + loses[k][i]:
-                    loses[j][i] = loses[j][k] + loses[k][i]
-    
-    for a in range(n):
-        win = 0
-        lose = 0
-        for b in range(n):
-            if a != b:
-                if wins[a][b] != INF:
-                    win += 1
-                if loses[a][b] != INF:
-                    lose += 1
-        if win + lose == n-1:
-            answer += 1
+    for i in range(1, n+1):
+        if len(win[i]) + len(lose[i]) == n - 1: answer += 1
                 
     return answer
