8b0bb5fe023a15db3cde4d6985d161646ca3c57a
Hyemi <78525791+5a6io@users.noreply.github.com>
2025-10-22 22:30:04 +0900

    [level 3] Title: 입국심사, Time: 617.01 ms, Memory: 13.6 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,11 +1,13 @@
 def solution(n, times):
     answer = 0
-    s, e = 1, n*min(times)
+    times.sort()
+    s, e = times[0], n*times[0]
     while s <= e:
         m = (s + e) // 2
         p = 0
         for time in times:
-            p += m//time
+            p += m // time
+        
         if p >= n:
             answer = m
             e = m - 1
