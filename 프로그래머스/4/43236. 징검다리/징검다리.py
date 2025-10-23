eddcc81f1ef4f69ea7be74d2d8b1885ffd75b9bd
Hyemi <78525791+5a6io@users.noreply.github.com>
2025-10-23 22:59:14 +0900

    [level 4] Title: 징검다리, Time: 136.95 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.py
+++ 프로그래머스/4/43236. 징검다리/징검다리.py
@@ -5,16 +5,13 @@ def solution(distance, rocks, n):
     s, e = 0, distance
     while s <= e:
         m = (s + e) // 2
-        md = int(1e9) # 각 지점 사이의 최소 거리
         cr = 0 # 현재 돌
         cnt = 0 # 제거한 바위 개수
         for rock in rocks:
-            diff = rock - cr
-            if diff < m:
+            if rock - cr < m:
                 cnt += 1
             else:
                 cr = rock
-                md = min(md, diff)
         if cnt > n:
             e = m - 1
         else:
