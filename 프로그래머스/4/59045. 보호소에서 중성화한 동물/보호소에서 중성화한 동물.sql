bf410433c56337bc4cd0b032743cc1acc19b057c
Hyemi <78525791+5a6io@users.noreply.github.com>
2025-10-29 17:10:18 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -1,6 +1,12 @@
-select animal_id, animal_type, name
-from animal_outs
-where animal_id in (select animal_id
-                       from animal_ins
-                       where sex_upon_intake like 'Intact%') and
-                       (sex_upon_outcome like 'Spayed%' or sex_upon_outcome like 'Neutered%')
+# 서브쿼리를 이용한 풀이
+# select animal_id, animal_type, name
+# from animal_outs
+# where animal_id in (select animal_id
+#                        from animal_ins
+#                        where sex_upon_intake like 'Intact%') and
+#                        (sex_upon_outcome like 'Spayed%' or sex_upon_outcome like 'Neutered%')
+
+# join을 이용한 풀이
+select o.animal_id, o.animal_type, o.name
+from animal_outs o inner join animal_ins i on o.animal_id = i.animal_id
+where sex_upon_intake like 'Intact%' and sex_upon_outcome not like 'Intact%'
