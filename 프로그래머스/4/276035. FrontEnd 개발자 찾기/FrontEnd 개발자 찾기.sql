41c3b7b9d0d52cff555ca04e1c8e126315bbe9d0
Hyemi <78525791+5a6io@users.noreply.github.com>
2025-10-31 11:45:09 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -1,6 +1,4 @@
 select id, email, first_name, last_name
 from developers
-where skill_code & (select sum(code)
-                   from skillcodes
-                   where category = 'Front End')
+where skill_code&(select sum(code) from skillcodes where category='Front End')
 order by id
