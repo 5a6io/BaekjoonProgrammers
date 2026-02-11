d80f84856cf4ff84e019b2f3fe28defeaf9331c7
Hyemi <78525791+5a6io@users.noreply.github.com>
2026-02-11 19:58:35 +0900

    [level 3] Title: 등굣길, Time: 0.93 ms, Memory: 54.2 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,25 +1,32 @@
 import java.util.*;
 
 class Solution {
+    
+    static final int MOD = 1000000007;
     public int solution(int m, int n, int[][] puddles) {
-        int[][] dp = new int[n + 1][m + 1];
+        int answer = 0;
+        int[][] dp = new int[n][m];
         
-        for(int[] p : puddles)
-            dp[p[1]][p[0]] = -1;
+        for (int[] p : puddles){
+            dp[p[1]-1][p[0]-1] = -1;
+        }
         
-        dp[1][1] = 1;
-        for(int i = 1; i < n + 1; i++){
-            for(int j = 1; j < m + 1; j++){
-                if(dp[i][j] != -1){
-                    if(dp[i - 1][j] > 0)
-                        dp[i][j] = (dp[i][j] + dp[i - 1][j]) % 1000000007;
-                    if(dp[i][j - 1] > 0)
-                        dp[i][j] = (dp[i][j] + dp[i][j - 1]) % 1000000007;
-                }
-                
+        if (dp[0][0] == -1) return 0;
+        
+        dp[0][0] = 1;
+        
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < m; j++){
+                if (dp[i][j] == -1){
+                    dp[i][j] = 0;
+                    continue;
+                } 
+                if (i > 0) dp[i][j] = (dp[i][j] + dp[i-1][j])%MOD;
+                if (j > 0) dp[i][j] = (dp[i][j] + dp[i][j-1])%MOD;
             }
         }
         
-        return dp[n][m];
+        answer = dp[n-1][m-1];
+        return answer;
     }
 }
