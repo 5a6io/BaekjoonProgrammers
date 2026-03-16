ec9776bfd78b43bde7e0db8ca99d4518a1b465f1
Hyemi <78525791+5a6io@users.noreply.github.com>
2026-03-16 15:29:59 +0900

    [level 1] Title: 중요한 단어를 스포 방지, Time: 18.92 ms, Memory: 9.69 MB -BaekjoonHub

--- 프로그래머스/1/468370. 중요한 단어를 스포 방지/중요한 단어를 스포 방지.py
+++ 프로그래머스/1/468370. 중요한 단어를 스포 방지/중요한 단어를 스포 방지.py
@@ -1,57 +1,33 @@
-def find_unimportant(message, spoiler_ranges):
-    l = len(message)
+def solution(message, spoiler_ranges):
+    answer = 0
+    status = [0] * len(message)
+    important = set()
     unimportant = set()
-    status = [0] * l
     
     for s, e in spoiler_ranges:
         for i in range(s, e+1):
-            if 0 <= i < l:
-                status[i] = 1
+            status[i] = 1
     
-    i = 0
-    while i < l:
-        if message[i].isspace():
-            i += 1
-            continue
-        
-        s = i
-        while i < l and not message[i].isspace():
-            i += 1
-        
-        e = i-1
-        spoiler = False
-        for j in range(s, e+1):
-            if status[j]:
-                spoiler = True
+    words = []
+    idx = 0
+    for i, m in enumerate(message):
+        if m == ' ':
+            words.append((message[idx:i], idx, i-1))
+            idx = i+1
+    words.append((message[idx:], idx, len(message)-1))
+    
+    for w, s, e in words:
+        for i in range(s, e+1):
+            if status[i]:
                 break
-        if not spoiler:
-            unimportant.add("".join(message[s:e+1]))
+        else:
+            unimportant.add(w)
+            
+    for start, end in spoiler_ranges:
+        for w, s, e in words:
+            if not (e < start or end < s):
+                if w not in unimportant:
+                    important.add(w)
     
-    return unimportant
-    
-def find_important(s, e, message):
-    l = len(message)
-    start = s
-    end = e
-    if start < 0 or end >= l or start > end:
-        return ""
-        
-    while start > 0 and not message[start - 1].isspace():
-        start -= 1
-        
-    while end < l - 1 and not message[end + 1].isspace():
-        end += 1
-        
-    return "".join(message[start:end+1])
-
-def solution(message, spoiler_ranges):  
-    unimportant = find_unimportant(message, spoiler_ranges)
-    important = set()
-                            
-    for s, e in spoiler_ranges:
-        word = find_important(s, e, message)
-        for w in word.split():
-            if w not in unimportant:
-                important.add(w)
-                
-    return len(important)
+    answer = len(important)
+    return answer
